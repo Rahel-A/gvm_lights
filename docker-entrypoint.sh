65dcef946955aa04e0a2ac01373bfe4e63624541
Rahel Ahmed <rahel@tuta.io>
2022-06-26 18:41:21 +0100

    Only print list of local devices under trace level logging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,15 @@
 
 service dbus start
 bluetoothd &
-bluetoothctl list
 bluetoothctl power off
+sleep 1
 bluetoothctl power on
 sleep 1
+
+if [ "$RUST_LOG" == trace ]
+then
+    bluetoothctl list
+fi
+
 
 /usr/bin/gvm_lights --server
