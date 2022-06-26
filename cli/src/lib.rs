0f51ec301a1e9618cf63a9ddd808d419d19bb732
Rahel Ahmed <rahel@tuta.io>
2022-06-26 13:32:55 +0100

    Refactor CLI related functions to lib

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,2 +1,106 @@
+use clap::{Arg, Command, PossibleValue, ArgMatches};
+use gvm_lights::{ControlMessage, LightCmd, ModeCmd};
+
 pub mod client;
 pub use client::Client;
+
+pub fn find_command(matches: &ArgMatches) -> Option<ControlMessage> {
+    if let Some(s) = matches.get_one::<String>("light") {
+        Some(match s.as_str() {
+            "on" => ControlMessage::Light(LightCmd::On),
+            "off" => ControlMessage::Light(LightCmd::Off),
+            _ => panic!("Incorrect argument passed")
+        })
+    } else if let Some(s) = matches.get_one::<String>("mode") {
+        Some(match s.as_str() {
+            "CT" => ControlMessage::Mode(ModeCmd::ColourTemp),
+            "HS" => ControlMessage::Mode(ModeCmd::HueSat),
+            "Sc" => ControlMessage::Mode(ModeCmd::Scenes),
+            _ => panic!("Incorrect argument passed")
+        })
+    } else if let Some(br) = matches.get_one::<u8>("brightness") {
+        Some(ControlMessage::Brightness(*br))
+    } else if let Some(t) = matches.get_one::<u16>("temperature") {
+        Some(ControlMessage::Temperature(*t))
+    } else if let Some(h) = matches.get_one::<u16>("hue") {
+        Some(ControlMessage::Hue(*h))
+    } else if let Some(sat) = matches.get_one::<u8>("saturation") {
+        Some(ControlMessage::Saturation(*sat))
+    } else if let Some(sc) = matches.get_one::<u8>("scene") {
+        Some(ControlMessage::Scene(*sc))
+    } else if let Some(r) = matches.get_one::<u8>("rgb") {
+        Some(ControlMessage::RGB(*r))
+    } else if matches.contains_id("state") {
+        Some(ControlMessage::ReadState())
+    } else {
+        None
+    }
+}
+
+pub fn cli() -> Command<'static> {
+    let validator_u8 = clap::value_parser!(u8);
+    let validator_u16 = clap::value_parser!(u16);
+
+    Command::new("GVM Lights")
+        .version("0.1.0")
+        .arg(Arg::new("light")
+                  .long("light")
+                  .short('l')
+                  .takes_value(true)
+                  .value_parser([
+                      PossibleValue::new("on"),
+                      PossibleValue::new("off")]))
+        .arg(Arg::new("brightness")
+                  .long("brightness")
+                  .short('b')
+                  .value_parser(validator_u8)
+                  .takes_value(true))
+        .arg(Arg::new("temperature")
+                  .long("temperature")
+                  .short('t')
+                  .value_parser(validator_u16)
+                  .takes_value(true))
+        .arg(Arg::new("hue")
+                  .long("hue")
+                  .short('h')
+                  .value_parser(validator_u16)
+                  .takes_value(true))
+        .arg(Arg::new("saturation")
+                  .long("saturation")
+                  .short('s')
+                  .value_parser(validator_u8)
+                  .takes_value(true))
+        .arg(Arg::new("mode")
+                  .long("mode")
+                  .short('m')
+                  .takes_value(true)
+                  .value_parser([
+                      PossibleValue::new("CT"),
+                      PossibleValue::new("HS"),
+                      PossibleValue::new("Sc")]))
+        .arg(Arg::new("scene")
+                  .long("scene")
+                  .short('z')
+                  .value_parser(validator_u8)
+                  .takes_value(true))
+        .arg(Arg::new("rgb")
+                  .long("rgb")
+                  .short('r')
+                  .value_parser(validator_u8)
+                  .takes_value(true))
+        .arg(Arg::new("server")
+                  .long("server")
+                  .short('x')
+                  .takes_value(false))
+        .arg(Arg::new("state")
+                  .long("state")
+                  .short('i')
+                  .takes_value(false))
+        .arg(Arg::new("client")
+                  .long("client")
+                  .default_value("255")
+                  .value_parser(validator_u8)
+                  .short('c')
+                  .takes_value(true))
+}
+
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,4 @@
-use clap::{Arg, Command, PossibleValue};
-use gvm_lights::{ControlMessage, LightCmd, ModeCmd};
+use gvm_lights::ControlMessage;
 use log::info;
 use dotenv::dotenv;
 use gvm_server::Server;
@@ -7,21 +6,6 @@ use gvm_cli::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // argument strings
-    let brightness = "brightness";
-    let temperature = "temperature";
-    let hue = "hue";
-    let saturation = "saturation";
-    let server = "server";
-    let light = "light";
-    let scene = "scene";
-    let mode = "mode";
-    let rgb = "rgb";
-    let client = "client";
-    let state = "state";
-    let validator_u8 = clap::value_parser!(u8);
-    let validator_u16 = clap::value_parser!(u16);
-
     let address = format!("{}:{}",
         dotenv::var("APP_HOST").unwrap_or("0.0.0.0".to_string()),
         dotenv::var("APP_PORT").unwrap_or("8631".to_string()));
@@ -29,70 +13,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
     dotenv().ok();
 
-    let matches = Command::new("GVM Lights")
-        .version("0.1.0")
-        .arg(Arg::new(light)
-                  .long(light)
-                  .short('l')
-                  .takes_value(true)
-                  .value_parser([
-                      PossibleValue::new("on"),
-                      PossibleValue::new("off")]))
-        .arg(Arg::new(brightness)
-                  .long(brightness)
-                  .short('b')
-                  .value_parser(validator_u8)
-                  .takes_value(true))
-        .arg(Arg::new(temperature)
-                  .long(temperature)
-                  .short('t')
-                  .value_parser(validator_u16)
-                  .takes_value(true))
-        .arg(Arg::new(hue)
-                  .long(hue)
-                  .short('h')
-                  .value_parser(validator_u16)
-                  .takes_value(true))
-        .arg(Arg::new(saturation)
-                  .long(saturation)
-                  .short('s')
-                  .value_parser(validator_u8)
-                  .takes_value(true))
-        .arg(Arg::new(mode)
-                  .long(mode)
-                  .short('m')
-                  .takes_value(true)
-                  .value_parser([
-                      PossibleValue::new("CT"),
-                      PossibleValue::new("HS"),
-                      PossibleValue::new("Sc")]))
-        .arg(Arg::new(scene)
-                  .long(scene)
-                  .short('z')
-                  .value_parser(validator_u8)
-                  .takes_value(true))
-        .arg(Arg::new(rgb)
-                  .long(rgb)
-                  .short('r')
-                  .value_parser(validator_u8)
-                  .takes_value(true))
-        .arg(Arg::new(server)
-                  .long(server)
-                  .short('x')
-                  .takes_value(false))
-        .arg(Arg::new(state)
-                  .long(state)
-                  .short('i')
-                  .takes_value(false))
-        .arg(Arg::new(client)
-                  .long(client)
-                  .default_value("255")
-                  .value_parser(validator_u8)
-                  .short('c')
-                  .takes_value(true))
-        .get_matches();
+    let matches = gvm_cli::cli().get_matches();
 
-    if matches.is_present(server) {
+    if matches.is_present("server") {
         match dotenv::var("clients") {
             Ok(gvm_clients) => {
                 let mut server = Server::new(address, gvm_clients).await?;
@@ -102,38 +25,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
     }
     else {
-        let target = matches.get_one::<u8>(client);
-        let cmd =
-            if let Some(s) = matches.get_one::<String>(light) {
-                match s.as_str() {
-                    "on" => ControlMessage::Light(LightCmd::On),
-                    "off" => ControlMessage::Light(LightCmd::Off),
-                    _ => panic!("Incorrect argument passed")
-                }
-            } else if let Some(s) = matches.get_one::<String>(mode) {
-                match s.as_str() {
-                    "CT" => ControlMessage::Mode(ModeCmd::ColourTemp),
-                    "HS" => ControlMessage::Mode(ModeCmd::HueSat),
-                    "Sc" => ControlMessage::Mode(ModeCmd::Scenes),
-                    _ => panic!("Incorrect argument passed")
-                }
-            } else if let Some(br) = matches.get_one::<u8>(brightness) {
-                ControlMessage::Brightness(*br)
-            } else if let Some(t) = matches.get_one::<u16>(temperature) {
-                ControlMessage::Temperature(*t)
-            } else if let Some(hue) = matches.get_one::<u16>(hue) {
-                ControlMessage::Hue(*hue)
-            } else if let Some(sat) = matches.get_one::<u8>(saturation) {
-                ControlMessage::Saturation(*sat)
-            } else if let Some(scene) = matches.get_one::<u8>(scene) {
-                ControlMessage::Scene(*scene)
-            } else if let Some(rgb) = matches.get_one::<u8>(rgb) {
-                ControlMessage::RGB(*rgb)
-            } else if matches.contains_id(state) {
-                ControlMessage::ReadState()
-            } else {
-                panic!("Not recognized command");
-            };
+        let target = matches.get_one::<u8>("client");
+        let cmd = gvm_cli::find_command(&matches).expect("No command detected in arguments");
         info!("Parsed arguments into: {:?}", cmd);
 
         let mut client = Client::new(address, *target.unwrap()).await?;
