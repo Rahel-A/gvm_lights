13108faa08b75b4b232b3188336304acd12668de
Rahel Ahmed <rahel@tuta.io>
2022-06-28 08:31:47 +0100

    Get broker server from environment instead

--- cli-py/tests/test.py
+++ cli-py/tests/test.py
@@ -1,6 +1,7 @@
 import asyncio
 import cli_py
 import time
+import os
 
 def main():
     run()
@@ -9,10 +10,13 @@ async def run():
     cli_py.initialise_log()
     test_off = cli_py.PyServerMessage("--light off")
     test_on = cli_py.PyServerMessage("--light on")
-    clients = await cli_py.new("10.139.21.199:8631")
+    clients = await cli_py.new(f"{APP_HOST}:{APP_PORT}")
     #await clients[0].send_message(test_off)
     t1 = await clients[0].get_state()
     t2 = await clients[1].get_state()
     return [t1, t2]
+
+APP_HOST = os.environ.get('APP_HOST', '0.0.0.0')
+APP_PORT = os.environ.get('APP_PORT', '8631')
 
 t = asyncio.run(run())
